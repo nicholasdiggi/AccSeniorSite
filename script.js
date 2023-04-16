23b49de8f0f32341d4ebfa9f8a05b99fb6f4a076
Dylen Greenenwald <gradyl16@protonmail.com>
2023-04-15 23:18:30 -0500

    commented out auth0 code to preserve functionality of tutorial

--- script.js
+++ script.js
@@ -1,62 +1,62 @@
 // Auth0 code taken from auth0 site
 // https://auth0.com/docs/quickstart/spa/vanillajs?framed=1&sq=1#initialize-the-sdk
 
-// Initialize the SDK
-let auth0Client = null;
+// // Initialize the SDK
+// let auth0Client = null;
 
-const fetchAuthConfig = () => fetch("/auth_config.json");
+// const fetchAuthConfig = () => fetch("/auth_config.json");
 
-const configureClient = async () => {
-  const response = await fetchAuthConfig();
-  const config = await response.json();
+// const configureClient = async () => {
+//   const response = await fetchAuthConfig();
+//   const config = await response.json();
 
-  auth0Client = await auth0.createAuth0Client({
-    domain: config.domain,
-    clientId: config.clientId
-  });
-};
+//   auth0Client = await auth0.createAuth0Client({
+//     domain: config.domain,
+//     clientId: config.clientId
+//   });
+// };
 
-window.onload = async () => {
-  await configureClient();
+// window.onload = async () => {
+//   await configureClient();
 
-  // update the UI state
-  updateUI();
+//   // update the UI state
+//   updateUI();
 
-  const isAuthenticated = await auth0Client.isAuthenticated();
+//   const isAuthenticated = await auth0Client.isAuthenticated();
 
-  if (isAuthenticated) {
-    // show the gated content
-    return;
-  }
+//   if (isAuthenticated) {
+//     // show the gated content
+//     return;
+//   }
 
-  // check for the code and state parameters
-  const query = window.location.search;
-  if (query.includes("code=") && query.includes("state=")) {
+//   // check for the code and state parameters
+//   const query = window.location.search;
+//   if (query.includes("code=") && query.includes("state=")) {
 
-    // Process the login state
-    await auth0Client.handleRedirectCallback();
-    
-    updateUI();
+//     // Process the login state
+//     await auth0Client.handleRedirectCallback();
 
-    // Use replaceState to redirect the user away and remove the querystring parameters
-    window.history.replaceState({}, document.title, "/");
-  }
-}
+//     updateUI();
 
-const updateUI = async () => {
-  const isAuthenticated = await auth0Client.isAuthenticated();
+//     // Use replaceState to redirect the user away and remove the querystring parameters
+//     window.history.replaceState({}, document.title, "/");
+//   }
+// }
 
-  document.getElementById("btn-logout").disabled = !isAuthenticated;
-  document.getElementById("btn-login").disabled = isAuthenticated;
-};
+// const updateUI = async () => {
+//   const isAuthenticated = await auth0Client.isAuthenticated();
 
-const login = async () => {
-  await auth0Client.loginWithRedirect({
-    authorizationParams: {
-      redirect_uri: window.location.origin
-    }
-  });
-};
+//   document.getElementById("btn-logout").disabled = !isAuthenticated;
+//   document.getElementById("btn-login").disabled = isAuthenticated;
+// };
+
+// const login = async () => {
+//   await auth0Client.loginWithRedirect({
+//     authorizationParams: {
+//       redirect_uri: window.location.origin
+//     }
+//   });
+// };
 
 // Get the modal
 var modal = document.getElementById("login-form");
@@ -68,34 +68,24 @@ var btn = document.querySelector("nav li.right a");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 }
 
-function emphasizeSignIn() {
-  // window.scrollTo(0, 0);
-  // var elements = document.querySelectorAll('body > *:not(nav) *:not(#first-step)');
-  // for (var i = 0; i < elements.length; i++) {
-  //   elements[i].classList.toggle('blur');
-  // }
-
-  // var firstStep = document.getElementById('first-step');
-  // var navElements = document.querySelectorAll('li');
-  // for (var i = 0; i < navElements.length; i++) {
-  //   if (navElements[i] != firstStep)
-  //   {
-  //     navElements[i].classList.toggle('blur');
-  //   }
-  // }
-
+function beginTutorial() {
   var firstStep = document.getElementById('first-step');
   var welcomeMessage = document.getElementById('welcome-message');
   var registerExplanation = document.getElementById('register-explanation');
+
+  console.log("Before toggle:");
+  console.log("firstStep: ", firstStep);
+  console.log("welcomeMessage: ", welcomeMessage);
+  console.log("registerExplanation: ", registerExplanation);
 
   // Show sign-in button
   firstStep.classList.toggle("hidden");
@@ -105,10 +95,16 @@ function emphasizeSignIn() {
 
   // Show the registration explanation
   registerExplanation.classList.toggle("hidden");
+
+  console.log("After toggle:");
+  console.log("firstStep: ", firstStep);
+  console.log("welcomeMessage: ", welcomeMessage);
+  console.log("registerExplanation: ", registerExplanation);
 }
 
+
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
