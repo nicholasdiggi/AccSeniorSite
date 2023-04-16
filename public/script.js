163e66ce87fdc57ab272466baf542eb8f94a3660
Nicholas Di Girolamo <nicholasdiggi@gmail.com>
2023-04-16 12:10:38 -0500

    Removed duplicate comment

--- public/script.js
+++ public/script.js
@@ -2,7 +2,6 @@
 // https://console.firebase.google.com/u/0/project/sitetrainer-1/overview
 
 // Import the functions you need from the SDKs you need
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
