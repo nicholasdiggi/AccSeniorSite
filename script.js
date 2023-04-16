8b5ade4b191928f94bdb1d9db74a2adf35eef503
Nicholas Di Girolamo <nicholasdiggi@gmail.com>
2023-04-16 00:00:33 -0500

    Installed packages and added Firebase SDK

--- script.js
+++ script.js
@@ -58,6 +58,31 @@
 //   });
 // };
 
+// Firebase code, taken from Firebase website
+// https://console.firebase.google.com/u/0/project/sitetrainer-1/overview
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCaxeM1OT_FA_ZF5srxhwy6Pk6mIr5HRJA",
+  authDomain: "sitetrainer-1.firebaseapp.com",
+  projectId: "sitetrainer-1",
+  storageBucket: "sitetrainer-1.appspot.com",
+  messagingSenderId: "635692315384",
+  appId: "1:635692315384:web:2b50d7207e7f68790ec10d",
+  measurementId: "G-KM83YD1LN0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 // Get the modal
 var modal = document.getElementById("login-form");
 
