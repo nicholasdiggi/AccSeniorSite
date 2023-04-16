b15fa19645d0e4384b74dbaae4b12ac6e3da16a1
NoodleSoup0 <106336347+NoodleSoup0@users.noreply.github.com>
2023-04-16 00:30:04 -0500

    Firebase

--- script.js
+++ script.js
@@ -1,3 +1,89 @@
+// Auth0 code taken from auth0 site
+// https://auth0.com/docs/quickstart/spa/vanillajs?framed=1&sq=1#initialize-the-sdk
+
+// // Initialize the SDK
+// let auth0Client = null;
+
+// const fetchAuthConfig = () => fetch("/auth_config.json");
+
+// const configureClient = async () => {
+//   const response = await fetchAuthConfig();
+//   const config = await response.json();
+
+//   auth0Client = await auth0.createAuth0Client({
+//     domain: config.domain,
+//     clientId: config.clientId
+//   });
+// };
+
+// window.onload = async () => {
+//   await configureClient();
+
+//   // update the UI state
+//   updateUI();
+
+//   const isAuthenticated = await auth0Client.isAuthenticated();
+
+//   if (isAuthenticated) {
+//     // show the gated content
+//     return;
+//   }
+
+//   // check for the code and state parameters
+//   const query = window.location.search;
+//   if (query.includes("code=") && query.includes("state=")) {
+
+//     // Process the login state
+//     await auth0Client.handleRedirectCallback();
+
+//     updateUI();
+
+//     // Use replaceState to redirect the user away and remove the querystring parameters
+//     window.history.replaceState({}, document.title, "/");
+//   }
+// }
+
+// const updateUI = async () => {
+//   const isAuthenticated = await auth0Client.isAuthenticated();
+
+//   document.getElementById("btn-logout").disabled = !isAuthenticated;
+//   document.getElementById("btn-login").disabled = isAuthenticated;
+// };
+
+// const login = async () => {
+//   await auth0Client.loginWithRedirect({
+//     authorizationParams: {
+//       redirect_uri: window.location.origin
+//     }
+//   });
+// };
+
+// Firebase code, taken from Firebase website
+// https://console.firebase.google.com/u/0/project/sitetrainer-1/overview
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCaxeM1OT_FA_ZF5srxhwy6Pk6mIr5HRJA",
+  authDomain: "sitetrainer-1.firebaseapp.com",
+  projectId: "sitetrainer-1",
+  storageBucket: "sitetrainer-1.appspot.com",
+  messagingSenderId: "635692315384",
+  appId: "1:635692315384:web:2b50d7207e7f68790ec10d",
+  measurementId: "G-KM83YD1LN0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
 // Get the modal
 var modal = document.getElementById("login-form");
 
@@ -8,37 +94,43 @@ var btn = document.querySelector("nav li.right a");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 }
 
-function emphasizeSignIn() {
-  window.scrollTo(0, 0);
-  var elements = document.querySelectorAll('body > *:not(nav) *:not(#first-step)');
-  for (var i = 0; i < elements.length; i++) {
-    elements[i].classList.toggle('blur');
-  }
-
+function beginTutorial() {
   var firstStep = document.getElementById('first-step');
-  var navElements = document.querySelectorAll('li');
-  for (var i = 0; i < navElements.length; i++) {
-    if (navElements[i] != firstStep)
-    {
-      navElements[i].classList.toggle('blur');
-    }
-  }
+  var welcomeMessage = document.getElementById('welcome-message');
+  var registerExplanation = document.getElementById('register-explanation');
 
-  // Prevent user from scrolling
-  // document.body.style.overflow = 'hidden';
+  console.log("Before toggle:");
+  console.log("firstStep: ", firstStep);
+  console.log("welcomeMessage: ", welcomeMessage);
+  console.log("registerExplanation: ", registerExplanation);
+
+  // Show sign-in button
+  firstStep.classList.toggle("hidden");
+
+  // Hide the welcome message
+  welcomeMessage.classList.toggle("hidden");
+
+  // Show the registration explanation
+  registerExplanation.classList.toggle("hidden");
+
+  console.log("After toggle:");
+  console.log("firstStep: ", firstStep);
+  console.log("welcomeMessage: ", welcomeMessage);
+  console.log("registerExplanation: ", registerExplanation);
 }
 
+
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
