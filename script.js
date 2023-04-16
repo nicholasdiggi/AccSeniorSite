05e6b8bce596e57511a1ea93b324ebe930b327f5
Nicholas Di Girolamo <nicholasdiggi@gmail.com>
2023-04-15 22:30:38 -0500

    More auth0 stuff

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 // Import the Auth0 SPA SDK
 // import { createAuth0Client } from '@auth0/auth0-spa-js';
 
+// Initialize the SDK
+let auth0Client = null;
+
+const fetchAuthConfig = () => fetch("/auth_config.json");
+
+const configureClient = async () => {
+  const response = await fetchAuthConfig();
+  const config = await response.json();
+
+  auth0Client = await auth0.createAuth0Client({
+    domain: config.domain,
+    clientId: config.clientId
+  });
+};
+
+window.onload = async () => {
+  await configureClient();
+}
+
 // Get the modal
 var modal = document.getElementById("login-form");
 
