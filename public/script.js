a45a1e6427957fa123a44c7920dab0cc5631844b
Nicholas Di Girolamo <nicholasdiggi@gmail.com>
2023-04-16 09:32:22 -0500

    added import of getAuth to script.js

--- public/script.js
+++ public/script.js
@@ -6,6 +6,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -23,6 +24,8 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
 
 // FirebaseUI Auth
 // https://firebase.google.com/docs/auth/web/firebaseui
