d21f1982b3730473b2c1b4dd15181bccb3945ffd
Nicholas Di Girolamo <nicholasdiggi@gmail.com>
2023-04-15 22:45:22 -0500

    add log in functionality

--- script.js
+++ script.js
@@ -21,6 +21,26 @@ window.onload = async () => {
 
   // update the UI state
   updateUI();
+
+  const isAuthenticated = await auth0Client.isAuthenticated();
+
+  if (isAuthenticated) {
+    // show the gated content
+    return;
+  }
+
+  // NEW - check for the code and state parameters
+  const query = window.location.search;
+  if (query.includes("code=") && query.includes("state=")) {
+
+    // Process the login state
+    await auth0Client.handleRedirectCallback();
+    
+    updateUI();
+
+    // Use replaceState to redirect the user away and remove the querystring parameters
+    window.history.replaceState({}, document.title, "/");
+  }
 }
 
 const updateUI = async () => {
@@ -28,6 +48,14 @@ const updateUI = async () => {
 
   document.getElementById("btn-logout").disabled = !isAuthenticated;
   document.getElementById("btn-login").disabled = isAuthenticated;
+};
+
+const login = async () => {
+  await auth0Client.loginWithRedirect({
+    authorizationParams: {
+      redirect_uri: window.location.origin
+    }
+  });
 };
 
 // Get the modal
