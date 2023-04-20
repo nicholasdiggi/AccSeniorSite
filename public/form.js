49b360b94cb206ca24d56e7643b236b1a8b70e69
Nicholas Di Girolamo <nicholasdiggi@gmail.com>
2023-04-19 23:30:58 -0500

    Uncommented user authentication stuff

--- public/form.js
+++ public/form.js
@@ -10,40 +10,40 @@ document.getElementById('log-btn').addEventListener('click', function(){
   
 });
 
-// document.getElementById('login-btn').addEventListener('click', function(){
-//   const loginEmail=document.getElementById("login-email").value;
-//   const loginPassword=document.getElementById("login-password").value;
+document.getElementById('login-btn').addEventListener('click', function(){
+  const loginEmail=document.getElementById("login-email").value;
+  const loginPassword=document.getElementById("login-password").value;
 
-//   signInWithEmailAndPassword(auth, loginEmail, loginPassword)
-//   .then((userCredential) => {
-//     const user = userCredential.user;
-//     document.getElementById('result-box').style.display="inline";
-//     document.getElementById('login-div').style.display="none";
-//     document.getElementById('result').style.display="Welcome Back <br>" + loginEmail + " was Login Successfully";
-//   })
-//   .catch((error) => {
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-//     document.getElementById('result-box').style.display="inline";
-//     document.getElementById('login-div').style.display="none";
-//     document.getElementById('result').style.display="Sorry! <br>" + errorMessage;
-//   });
+  signInWithEmailAndPassword(auth, loginEmail, loginPassword)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    document.getElementById('result-box').style.display="inline";
+    document.getElementById('login-div').style.display="none";
+    document.getElementById('result').style.display="Welcome Back <br>" + loginEmail + " was Login Successfully";
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    document.getElementById('result-box').style.display="inline";
+    document.getElementById('login-div').style.display="none";
+    document.getElementById('result').style.display="Sorry! <br>" + errorMessage;
+  });
 
-// });
+});
 
 
-// document.getElementById('login-btn').addEventListener('click', function(){
-//   const registerEmail="johndoe@gmail.com"
-//   const registerPassword="123P";
+document.getElementById('login-btn').addEventListener('click', function(){
+  const registerEmail="johndoe@gmail.com"
+  const registerPassword="123P";
 
-//   firebase.auth().createUserWithEmailAndPassword(registerEmail, registerPassword)
-//   .then((userCredential) => {
-//     const user = userCredential.user;
-//     console.log(user)
-//   })
-//   .catch((error) => {
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-//     console.log(errorMessage)
-//   });
-// });
+  firebase.auth().createUserWithEmailAndPassword(registerEmail, registerPassword)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    console.log(user)
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorMessage)
+  });
+});
